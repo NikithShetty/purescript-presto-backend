7de6e853401fed2c08d153f239659483ca978bdc
Nikith Shetty <nikith.shetty@juspay.in>
2019-07-30 19:47:35 +0530

    backward compatibility

---
+++ src/Presto/Backend/Utils/Encoding.purs
@@ -0,0 +1,5 @@
+module Presto.Backend.Utils.Encoding
+  ( module Presto.Core.Utils.Encoding
+  ) where
+
+import Presto.Core.Utils.Encoding (defaultDecode, defaultDecodeJSON, defaultEncode, defaultEncodeJSON, defaultEnumDecode, defaultEnumEncode)
